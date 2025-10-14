5f542121ed71fd02e3941c52dd829628a8690282
jn_xyp <jinan.xyp@gmail.com>
2025-10-13 19:45:48 -0500

    更新渡鸦对照表导入脚本，修正部分obf对照表内容

--- para_tranz/temporary_scripts/data_ingestion/deathfly_translation_mapping_ingestion.py
+++ para_tranz/temporary_scripts/data_ingestion/deathfly_translation_mapping_ingestion.py
@@ -30,9 +30,17 @@ def load_deathfly_data():
 
     for row in data[1:]:
         if '"' in row[0]:
+            if row[3] == '#N/A':
+                row[3] = ''
             valid_data.append(row)
 
     return valid_data
+
+def remove_deathfly_escaping(text: str) -> str:
+    # 渡鸦提供的对照表中，\n\t是转义字符，需要转换为真正的换行符和制表符
+    # '\"'是转义字符，需要转换为真正的双引号
+    text = text.replace('<\\n', '<').replace('>\\n', '>').replace('\\n', '\n').replace('\\t', '\t').replace('\\"', '"')
+    return text
 
 def convert_deathfly_csv_to_paratranz_mapping():
     valid_data = load_deathfly_data()
@@ -62,7 +70,7 @@ def convert_deathfly_csv_to_paratranz_mapping():
         include_strings = sorted(list(data))
         # 渡鸦提供的对照表中，\n\t是转义字符，需要转换为真正的换行符和制表符
         # '\"'是转义字符，需要转换为真正的双引号
-        include_strings = [s.replace('\\n', '\n').replace('\\t', '\t').replace('\\"', '"') for s in include_strings]
+        include_strings = [remove_deathfly_escaping(s) for s in include_strings]
 
         class_desc = {
             "path": class_name + ".class",
@@ -84,16 +92,35 @@ def add_translation_to_exported_strings():
     for row in valid_data:
         raw_labels = row[0].split(' - ')
         if len(raw_labels) == 3:
-            original_text = row[2]
+            original_text = remove_deathfly_escaping(row[2])
             class_name = raw_labels[0].split('.')[0].strip()
-
-            if original_text.strip() and row[3].strip():
-                class_to_data[class_name][original_text] = row[3]
+            class_to_data[class_name][original_text] = remove_deathfly_escaping(row[3])
 
     jar_file_items = [item for item in PARA_TRANZ_MAP.items if isinstance(item, JarMapItem) and item.path == JAR_NAME]
     jar_files = [JavaJarFile(**asdict(item), no_auto_load=False) for item in jar_file_items]
 
-    print(jar_files)
+    for jar_file in jar_files:
+        if jar_file.path == JAR_NAME:
+            for klass_name, klass in jar_file.class_files.items():
+                klass_name = klass_name.removesuffix('.class')
+                if klass_name in class_to_data:
+                    original_to_translation = class_to_data[klass_name]
+                    updated_strings = []
+                    for string_item in klass.get_strings():
+                        original_text = string_item.original
+                        if original_text in original_to_translation:
+                            new_translation = original_to_translation[original_text]
+                            if contains_chinese(new_translation) and new_translation != string_item.translation:
+                                string_item.stage = 1 # 标记为已翻译
+                                string_item.translation = new_translation
+                                updated_strings.append(string_item)
+                        else:
+                            logger.warning(f'类 {klass_name} 中的词条 "{original_text}" 不在对照表中，跳过')
+                    klass.update_strings(updated_strings)
+                else:
+                    logger.warning(f'类 {klass_name} 不在对照表中，跳过')
+
+            jar_file.save_json()
 
 if __name__ == '__main__':
     # 先运行下面这个函数，生成mapping文件
